8fa35f4f5b135c2291c81aeac0b171a4bebe7ead
Sebna Omar <omargr97@gmail.com>
2025-02-19 17:47:21 +0000

    API done

--- src/api/Index.js
+++ src/api/Index.js
@@ -3,9 +3,27 @@ import axios from "axios"; //RECUERDA IMPORTAR SIEMPRE LO QUE NECESITES USAR
 const API_URL = "http://localhost:5005";
 
 // READ (.GET) (Permite mostrar la información almacenada en el servidor)
-export const getItems = async () => {
+export const getFood = async () => {
   try {
-    const response = await API_URL.get("/items");
+    const response = await axios.get(`${API_URL}/food_locations`);
+    return response.data;
+  } catch (error) {
+    console.error("Error al obtener los datos:", error);
+    throw error;
+  }
+};
+export const getProducts = async () => {
+  try {
+    const response = await axios.get(`${API_URL}/products`);
+    return response.data;
+  } catch (error) {
+    console.error("Error al obtener los datos:", error);
+    throw error;
+  }
+};
+export const getGeneralTips = async () => {
+  try {
+    const response = await axios.get(`${API_URL}/general_tips`);
     return response.data;
   } catch (error) {
     console.error("Error al obtener los datos:", error);
@@ -14,9 +32,27 @@ export const getItems = async () => {
 };
 
 // CREATE (.POST) (Añadir un elemento al servidor)
-export const createItem = async (item) => {
+export const createFood = async (item) => {
   try {
-    const response = await API_URL.post("/items", item);
+    const response = await axios.post("/food_location", item);
+    return response.data;
+  } catch (error) {
+    console.error("Error al crear el elemento:", error);
+    throw error;
+  }
+};
+export const createProducts = async (item) => {
+  try {
+    const response = await axios.post("/products", item);
+    return response.data;
+  } catch (error) {
+    console.error("Error al crear el elemento:", error);
+    throw error;
+  }
+};
+export const createGeneralTips = async (item) => {
+  try {
+    const response = await axios.post("/general_tips", item);
     return response.data;
   } catch (error) {
     console.error("Error al crear el elemento:", error);
@@ -25,9 +61,27 @@ export const createItem = async (item) => {
 };
 
 // UPDATE (.PUT) (Permite modificar un elemento ya existente en el servidor)
-export const updateItem = async (id, updatedItem) => {
+export const updateFood = async (id, updatedFood) => {
   try {
-    const response = await API_URL.put(`/items/${id}`, updatedItem);
+    const response = await axios.put(`/food_location/${id}`, updatedFood);
+    return response.data;
+  } catch (error) {
+    console.error("Error al actualizar el elemento:", error);
+    throw error;
+  }
+};
+export const updateProducts = async (id, updateProducts) => {
+  try {
+    const response = await axios.put(`/updateProducts/${id}`, updateProducts);
+    return response.data;
+  } catch (error) {
+    console.error("Error al actualizar el elemento:", error);
+    throw error;
+  }
+};
+export const updateGeneralTips = async (id, updateGeneralTips) => {
+  try {
+    const response = await axios.put(`/general_tips/${id}`, updateGeneralTips);
     return response.data;
   } catch (error) {
     console.error("Error al actualizar el elemento:", error);
@@ -36,9 +90,27 @@ export const updateItem = async (id, updatedItem) => {
 };
 
 // DELETE (.DELETE) (Permite eliminar cualquier elemento del servidor)
-export const deleteItem = async (id) => {
+export const deleteFood = async (id) => {
   try {
-    const response = await API_URL.delete(`/items/${id}`);
+    const response = await axios.delete(`/food_location/${id}`);
+    return response.data;
+  } catch (error) {
+    console.error("Error al eliminar el elemento:", error);
+    throw error;
+  }
+};
+export const deleteProducts = async (id) => {
+  try {
+    const response = await axios.delete(`/products/${id}`);
+    return response.data;
+  } catch (error) {
+    console.error("Error al eliminar el elemento:", error);
+    throw error;
+  }
+};
+export const deleteGeneralTips = async (id) => {
+  try {
+    const response = await axios.delete(`/general_tips/${id}`);
     return response.data;
   } catch (error) {
     console.error("Error al eliminar el elemento:", error);
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,13 @@
 import axios from "axios";
 import React from "react";
+import { useEffect } from "react";
+import { getFood } from "../api/Index";
 
 const Home = () => {
+  useEffect(() => {
+    getFood().then((food) => console.log(food));
+  }, []);
+
   return <h1>Muestra algo por favor</h1>;
 };
 
